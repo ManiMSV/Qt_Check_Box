8464207276c8e9f056cc04336e750f5a5c4ca612
MANIMSV <manikandan.2002121@srec.ac.in>
2025-03-03 21:44:20 +0530

    Feat: Add build files and CMake configuration for console application

---
+++ console_app/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_console_app_FILE C:/Projects/mis/Cpp_Qt_projects/console_app/build/Desktop_Qt_6_10_0_MinGW_64_bit-Debug/console_app.exe)
+set(__QT_DEPLOY_TARGET_console_app_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_console_app_RUNTIME_DLLS C:/Qt/6.10.0/mingw_64/bin/Qt6Core.dll)
